bb0468c5589bc7381f6faa8673c021295395c79d
Wondereich236 <kiyotakaa591@gmail.com>
2025-12-02 13:43:33 +0300

    Update application-icon.sh

--- application-icon.sh
+++ application-icon.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 shopt -s nullglob
 
-# SatellaOS arkaplan dizinini oluştur
+# SatellaOS uygulama simgeleri dizinini oluştur
 sudo mkdir -p /usr/share/SatellaOS/application-icon/
 
-# Arkaplan dosyalarını kopyala
+# Uygulama simgelerini kopyala
 sudo cp ~/git-script/application-icon/*.{jpg,png} /usr/share/SatellaOS/application-icon/
 
 # Dosya izinlerini ayarla
 sudo chmod 655 /usr/share/SatellaOS/application-icon/*
 
-# Kullanıcı Pictures klasörüne sembolik link oluştur
-ln -s /usr/share/SatellaOS/application-icon "$(xdg-user-dir PICTURES)/application-icon" 2>/dev/null
+# Kullanıcı için satella-picture klasörünü oluştur
+mkdir -p ~/satella-picture
+
+# Bu klasöre sembolik link ekle
+ln -s /usr/share/SatellaOS/application-icon ~/satella-picture/application-icon 2>/dev/null
 
 echo "✔ Simgeler başarıyla yüklendi!"
